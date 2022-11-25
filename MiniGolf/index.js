2fd3a956e1d54564463374fc892e098273fa9597
NimbleGames <97319135+NimbleValley@users.noreply.github.com>
2022-11-25 14:07:46 -0600

    Courses are playable, download courses

---
+++ MiniGolf/index.js
@@ -0,0 +1,24 @@
+const gameTitle = document.querySelector("#game-title");
+const homeBorder = document.querySelector("#home-border");
+const playButton = document.querySelector("#play-button");
+const footer = document.querySelector("#footer");
+
+const fader = document.getElementById("fader");
+
+const tl = new TimelineMax();
+
+tl.fromTo(gameTitle, 1, { x: "-25vw", opacity: 0 }, { x: 0, ease: Power2.easeOut, opacity: 1 });
+tl.fromTo(homeBorder, 1, { y: "25vh", opacity: 0 }, { y: 0, ease: Power2.easeOut, opacity: 1 },"-=1");
+tl.fromTo(playButton, 1, { scale: 0.25, opacity: 0 }, { scale: 1, ease: Power2.easeOut, opacity: 1 },"-=0.5");
+tl.fromTo(footer, 1, { y: "5vh", opacity: 0 }, { y: 0, ease: Power2.easeOut, opacity: 1 }, "-=.75");
+
+async function toMenuScreen() {
+    fader.style.display = "block";
+    tl.fromTo(fader, 1, { x: "-100vw", opacity: 0 }, { x: 0, ease: Power2.easeOut, opacity: 1 });
+    await sleep(1000);
+    open("menu.html", "_self");
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
---
+++ MiniGolf/menu.js
@@ -0,0 +1,20 @@
+const fader = document.getElementById("fader");
+
+const tl = new TimelineMax();
+
+fader.style.display = "block";
+tl.fromTo(fader, 1, { y: 0, opacity: 1 }, { y: "100vh", ease: Power2.easeOut, opacity: 0 });
+
+const fileSelector = document.getElementById('file-selector');
+  fileSelector.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    var formData = new FormData();
+    formData.set("file", file);
+    console.log(formData)
+    window.localStorage.setItem('customCourse', fileList);
+});
+
+function startGame(course) {
+  window.localStorage.setItem('courseNumber', course);
+  open("game.html", "_self");
+}
